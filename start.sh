9c6ed205edebcc2bbcb7177dc20c9cfb880a438c
Toray Altas <toray.altas@gmail.com>
2018-12-19 12:02:06 -0800

    Lower heap size

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Start elasticsearch, sleep to give time for ES to bootup (there is something asynchronous about the bootup of ES via service)
 update-rc.d elasticsearch defaults 95 10
+ES_JAVA_OPTS=-Xms750m -Xmx750m
 service elasticsearch start
 
 for i in 1 2 3 4 5 6 7 8
